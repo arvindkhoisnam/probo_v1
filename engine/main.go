e749b7edbdfafaddbfd335288584fe9aab301888
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-02-16 23:56:11 +0530

    update

--- engine/main.go
+++ engine/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/arvindkhoisnam/go_probo_engine/packages"
 )
 
 func main(){
 	engine := packages.InitEngine()
-	// engine.CreateMint("AAPL")
-	// engine.CreateMint("TSLA")
-	// engine.CreateMint("AAPL")
-	// engine.CreateMint("NFLX")
+	engine.CreateMarket("AAPL")
 	engine.CreateUser("user1")
-	engine.CreateUser("user2")
-	engine.OnrampINR("user1",1000)
-	engine.OnrampINR("user3",1000)
-	engine.OnrampINR("user2",1000)
-	// fmt.Println(engine)
+	engine.OnrampINR("user1",10000)
+	all := engine.AllMarkets()
+	fmt.Println(all)
+	fmt.Println("---------------------")
+	engine.Mint("user1","AAPL",5,1000)
+	engine.GetInrBal("user1")
+	engine.GetStockBal("user1")
 }
--- engine/packages/engine.go
+++ engine/packages/engine.go
@@ -59,24 +59,19 @@ func InitEngine() *Engine {
 	return engineInstance
 }
 
-func (e *Engine)CreateMint(stock string){
-	for _, val := range e.Orderbook {
-		if val.StockSymbol == stock{
-			fmt.Println("ALREADY EXISTS")
+func (e *Engine)CreateMarket(stock string){
+	if exists := e.checkStock(stock);exists{
+		fmt.Println("ALREADY EXISTS")
 			return
-		}
 	}
-	
 	orderbook := CreateOrderbook(stock)
 	e.Orderbook = append(e.Orderbook, *orderbook)
 }
 
 func (e *Engine)CreateUser(userId string){
-	_,alreadyExists := e.InrBalance.User[userId]
-	if (alreadyExists) {
+	if exists := e.checkUser(userId);exists{
 		fmt.Println("User already created.")
-		return
-	} 
+	}
 		 e.InrBalance.User[userId] = userBalance{
 			Balance: 0,
 			Locked: 0,
@@ -88,11 +83,119 @@ func (e *Engine)CreateUser(userId string){
 }
 
 func (e *Engine)OnrampINR(userId string, amount int){
-	user,userExists := e.InrBalance.User[userId]
-	if (!userExists){
+	if exists := e.checkUser(userId); !exists{
 		fmt.Println("No user found. Please create user.")
 		return
 	}
+	user := e.InrBalance.User[userId]
 	user.Balance += amount
+	e.InrBalance.User[userId] = user
 	fmt.Println(user)
+}
+
+func (e *Engine)GetInrBal(userId string) {
+	if exists := e.checkUser(userId);!exists{
+		fmt.Println("User does not exist")
+	}
+
+	userBal := e.InrBalance.User[userId]
+	fmt.Println(userBal)
+}
+
+func (e *Engine)GetStockBal(userId string) {
+	if exists := e.checkUser(userId);!exists{
+		fmt.Println("User does not exist")
+	}
+
+	userStocks := e.StockBalance.User[userId]
+	fmt.Println(userStocks)
+}
+type market struct{
+	stockSymbol string
+	currYesPrice int
+	currNoPrice int
+}
+func (e *Engine)AllMarkets()[]market{
+	var allMarkets []market
+	for _, val := range e.Orderbook{
+		m := market{
+			stockSymbol: val.StockSymbol,
+			currYesPrice: val.CurrYesPrice,
+			currNoPrice: val.CurrNoPrice,
+		}
+		allMarkets = append(allMarkets,m)
+	}
+	return allMarkets
+}
+
+func (e *Engine)Mint(userId, stock string, qty,price int){
+	if exists := e.checkStock(stock); !exists{
+		fmt.Printf("Market does not exist for %s stock.\n",stock)
+		return
+	}
+
+	if exists := e.checkUser(userId); !exists{
+		fmt.Println("No user found. Please create user.")
+		return
+	}
+
+	if sufficient := e.checkInrBal(userId,qty*price); !sufficient {
+		fmt.Println("Insufficient balance")
+		return
+	}
+
+	userStocks := e.StockBalance.User[userId]
+	userStocks.Symbol = map[string]stockType{
+		stock: {
+			Type: map[StockType]quantity{
+				YesStock: {Available: qty, Locked: 0},
+				NoStock:  {Available: qty, Locked: 0},
+			},
+		},
+	}
+	e.StockBalance.User[userId] = userStocks
+
+	userBal := e.InrBalance.User[userId]
+	userBal.Balance -= qty*price
+	e.InrBalance.User[userId] = userBal
+}
+
+
+func (e *Engine)PlaceOrder(userId,stock,stockType,orderType string, quantity,price int){
+	if exists := e.checkStock(stock); !exists{
+		fmt.Printf("Market does not exist for %s stock.\n",stock)
+		return
+	}
+
+
+}
+
+// Helper function
+func (e *Engine) checkUser(userId string)  bool {
+	_, exists := e.InrBalance.User[userId]
+	return exists
+}
+
+func (e *Engine)checkStock(stock string)bool{
+	for _, val := range e.Orderbook {
+		if val.StockSymbol == stock{
+			return true
+		}
+	}
+	return false
+}
+
+func (e *Engine)checkInrBal(userId string, request int)bool{
+	user := e.InrBalance.User[userId]
+	if user.Balance >= request {
+		return user.Balance >= request
+	}
+	return false
+}
+
+func (e *Engine)checkAndLockStock(userId,stock,stockType string, quantity int ){
+	if userStocks,exists := e.StockBalance.User[userId]; !exists {
+		fmt.Printf("You do not have %s stocks",stock)
+		return
+	}
 }
